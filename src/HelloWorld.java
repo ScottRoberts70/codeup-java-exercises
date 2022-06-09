42f243b5bbaa0e6a8f3e23adcd692c1723f1dbee
Scott Roberts <scott.w.roberts70@gmail.com>
2022-06-09 15:41:47 -0500

    Java practice exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,57 @@
+import org.w3c.dom.ls.LSOutput;
+
 public class HelloWorld {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         System.out.println("Hello, World");
+
+
+        int FavoriteNumber = 8;
+        System.out.println(FavoriteNumber);
+
+         double myString = 3.14159;
+        System.out.println(myString);
+
+        double myNumber = 3.14;
+        System.out.println(myNumber);
+//changed to double to make it work, so I could move on.
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+        //printed 5 and then 6 because X came before the ++
+
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+        //6 and 6 because the ++ was first then X was second, so we added 1
+
+//        final class = ;
+//        turns orange because its a reserved word.
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+        //Exception in thread "main" java.lang.ClassCastException: java.lang.String incompatible with java.lang.Integer
+        //	at java.base/java.lang.ClassCastException.<init>(ClassCastException.java:71)
+        //	at HelloWorld.main(HelloWorld.java:33)
+        //the errors are we tryed assgining the number 3 as three to an int. and then called int instead of the class three.
+
+//        int x = 4;
+//        System.out.println(x+=5);
+
+
+//        int x = 3;
+//        int y = 4;
+//        y = y *= x ;
+
+//        int x = 10;
+//        int y = 2;
+//        x = x /= y;
+//        y = y -= x;
+
+
     }
+
 }
+
+
