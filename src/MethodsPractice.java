080ddc92a7e349378a593d59e0b418d719d9b242
Scott Roberts <scott.w.roberts70@gmail.com>
2022-06-13 12:25:10 -0500

    MethodsInClassPractice

---
+++ src/MethodsPractice.java
@@ -0,0 +1,12 @@
+public class MethodsPractice {
+
+    public static int getDifference(int num1, int num2) {
+        return num1 - num2;   // If you don't use a return we can use static void instead of int. Using int is better though
+    }
+    public static void main(String[] args) {
+        System.out.println(getDifference(10, 5));
+    }
+}
+
+
+
