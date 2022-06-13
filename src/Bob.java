257fe89634eb644d22533b222ab006ea8f9f4f0f
Scott Roberts <scott.w.roberts70@gmail.com>
2022-06-13 10:27:42 -0500

    refactores the loop in BobExercise

--- src/Bob.java
+++ src/Bob.java
@@ -2,23 +2,26 @@ import java.util.Scanner;
 
 public class Bob {
     public static void main(String[] args) {
-        String ans;
-        do {
-        Scanner input = new Scanner(System.in);
-        System.out.println("Say something to Bob: ");
-        String answer = input.nextLine();
-        if(answer.endsWith("?")) {
-            System.out.println("Sure");
-        } else if (answer.endsWith("!")) {
-            System.out.println("Whoa chill out!");
-        } else if (answer.isEmpty()) {
-        System.out.println("Fine by the way!");
-        }else {
-            System.out.println("Whatever.");
-        }
+        boolean decision = true;
+        while (decision) {
+            Scanner input = new Scanner(System.in);
+            System.out.println("Say something to Bob: ");
+            String answer = input.nextLine();
+            if (answer.endsWith("?")) {
+                System.out.println("Sure");
+            } else if (answer.endsWith("!")) {
+                System.out.println("Whoa chill out!");
+            } else if (answer.isEmpty()) {
+                System.out.println("Fine by the way!");
+            } else {
+                System.out.println("Whatever.");
+            }
             System.out.println("Keep talking to Bob? [y] or [n]");
-            ans = input.next();
-        } while (!ans.equals("n"));
+            String userChoice = input.next();
+            if (userChoice.equalsIgnoreCase("n")) {
+                decision = false;
+            }
         }
     }
+}
 
