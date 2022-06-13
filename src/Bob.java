6c4e0b74515e1242118e50c3fb8f52e16d824d86
Scott Roberts <scott.w.roberts70@gmail.com>
2022-06-12 19:43:26 -0500

    started Bob Exercise

---
+++ src/Bob.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Bob {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Say something to Bob: ");
+        String answer = input.nextLine();
+
+    }
+}
