e158c26c5e9d096f593d85981a22971154d20f11
Scott Roberts <scott.w.roberts70@gmail.com>
2022-06-13 07:31:33 -0500

    Finished the Bob Exercise

--- src/Bob.java
+++ src/Bob.java
@@ -5,6 +5,14 @@ public class Bob {
         Scanner input = new Scanner(System.in);
         System.out.println("Say something to Bob: ");
         String answer = input.nextLine();
-
+        if(answer.endsWith("?")) {
+            System.out.println("Sure");
+        } else if (answer.endsWith("!")) {
+            System.out.println("Whoa chill out!");
+        } else if (answer.isEmpty()) {
+        System.out.println("Fine by the way!");
+        }else {
+            System.out.println("Whatever.");
+        }
     }
 }
