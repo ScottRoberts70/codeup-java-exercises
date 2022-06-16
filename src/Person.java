762a6dd2e7122fd10c30bcc2dcd46c20dccc79fb
Scott Roberts <scott.w.roberts70@gmail.com>
2022-06-16 13:31:40 -0500

    OOP Exercise #1

---
+++ src/Person.java
@@ -0,0 +1,29 @@
+public class Person {
+    private String name;
+
+    public String getName(){
+    return name;
+    }
+
+    public void setName(String name){
+    this.name = name;
+    }
+
+    public String sayHello() {
+//TODO: print a message to the console using the person's name
+        return String.format("Hello %s !", this.name);
+    }
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public static void main(String[] args) {
+        Person scott = new Person("Scott");
+        System.out.println(scott.getName());
+        System.out.println(scott.sayHello());
+
+    }
+
+}
+
