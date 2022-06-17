0bfc54ea77b7b14aa4ccfe28806db47a7db1706f
Scott Roberts <scott.w.roberts70@gmail.com>
2022-06-17 09:11:00 -0500

    added the final step in circle exercise

--- src/shapes/CircleApp.java
+++ src/shapes/CircleApp.java
@@ -4,20 +4,27 @@ package shapes;
 import com.sun.jdi.PathSearchingVirtualMachine;
 import util.Input;
 
+import java.util.IllegalFormatCodePointException;
+
 public class CircleApp {
 
     public static void main(String[] args) {
+        int count = 0;
+
         while (true) {
-
-        Input input = new Input();
-        Circle circle = new Circle(input.getDouble());
-        System.out.println(circle.getRadius());
-        System.out.println(circle.getCircumference());
-        System.out.println(circle.getArea());
-        if (input.yesNo()){
-
-        }
-//        System.out.println(input.yesNo());
+            Input input = new Input();
+            Circle circle = new Circle(input.getDouble());
+            Double rad = circle.getRadius();
+            System.out.println("The radius of your circle is: " +rad);
+            Double circum = circle.getCircumference();
+            System.out.println("The circumference is " +circum);
+            Double area = circle.getArea();
+            System.out.println("The area of your circle is :" +area);
+            count++;
+            if (input.yesNo() == false) {
+                System.out.println("You have made " + count + " circles!");
+                break;
+            }
         }
     }
 
